ce3edb159b2aa9ef3928e4ecaccf085574fc5ab7
sahoorajesh <rajsahoo2441@gmail.com>
2022-02-08 00:15:03 +0530

    Toggle menu Added

--- proctoring-system/src/Components/SideNavbar.js
+++ proctoring-system/src/Components/SideNavbar.js
@@ -1,76 +1,134 @@
 
+import { useState } from 'react'
 import '../ComponentCSS/SideNavbar.css'
 // import '../ComponentJS/SideNavbarJS'
 const SideNavbar = () => {
-    
+    var [classname, setclassname] = useState("dashboard-nav-dropdown");
+
+    var [show, setshow] = useState(false);
+
+    const handleshow = () => {
+        setshow(!show);
+        if (show) {
+            setclassname("dashboard-nav-dropdown show ")
+        }
+        else {
+            setclassname("dashboard-nav-dropdown ")
+        }
+        console.log(classname);
+    }
+
+
     return (
-        <div class='dashboard'>
-            <div class="dashboard-nav">
-                <header><a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a><a href="#"
-                    class="brand-logo"><i
-                        class="	fas fa-bullseye"></i> <span>PROCTOROR</span></a></header>
-                <nav class="dashboard-nav-list"><a href="#" class="dashboard-nav-item"><i class="fas fa-home"></i>
-            Home </a>
-            <a
-                        href="#" class="dashboard-nav-item"><i class="fas fa-user"></i> Profile </a>
-            <a
-                        href="#" class="dashboard-nav-item active"><i class="fas fa-plus"></i> Create Exam
-        </a>
-        <a
-                        href="#" class="dashboard-nav-item active"><i class="fas fa-eye"></i> View Exam
-        </a>
-        <a
-                        href="#" class="dashboard-nav-item active"><i class="fas fa-pencil-alt"></i> Update Exam
-        </a>
-        <a
-                        href="#" class="dashboard-nav-item active"><i class="fas fa-trash"></i> Delete Exam
-        </a>
-        <a
-                        href="#" class="dashboard-nav-item"><i class="fas fa-video"></i> Monitor Exam </a>
-                        <a
-                        href="#" class="dashboard-nav-item active"><i class="fas fa-clipboard-check"></i> Publish Result
-        </a>
-                    <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i
-                        class="fas fa-photo-video"></i> Media </a>
-                        <div class='dashboard-nav-dropdown-menu'><a href="#"
-                            class="dashboard-nav-dropdown-item">All</a><a
-                                href="#" class="dashboard-nav-dropdown-item">Recent</a><a
-                                    href="#" class="dashboard-nav-dropdown-item">Images</a><a
-                                        href="#" class="dashboard-nav-dropdown-item">Video</a></div>
-                    </div>
-                    <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i
-                        class="fas fa-users"></i> Users </a>
-                        <div class='dashboard-nav-dropdown-menu'><a href="#"
-                            class="dashboard-nav-dropdown-item">All</a><a
-                                href="#" class="dashboard-nav-dropdown-item">Subscribed</a><a
-                                    href="#"
-                                    class="dashboard-nav-dropdown-item">Non-subscribed</a><a
-                                        href="#" class="dashboard-nav-dropdown-item">Banned</a><a
-                                            href="#" class="dashboard-nav-dropdown-item">New</a></div>
-                    </div>
-                    <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i
-                        class="fas fa-money-check-alt"></i> Payments </a>
-                        <div class='dashboard-nav-dropdown-menu'><a href="#"
-                            class="dashboard-nav-dropdown-item">All</a><a
-                                href="#" class="dashboard-nav-dropdown-item">Recent</a><a
-                                    href="#" class="dashboard-nav-dropdown-item"> Projections</a>
+        <div className='dashboard'>
+            <div className="dashboard-nav">
+                <header><a className="menu-toggle"><i className="fas fa-bars"></i></a>
+                    <a className="brand-logo">
+                        <i className="	fas fa-bullseye"></i> <span>PROCTOROR</span></a></header>
+
+                <nav className="dashboard-nav-list">
+                    <a className="dashboard-nav-item">
+                        <i className="fas fa-home"></i>
+                        Home
+                    </a>
+                    <a className="dashboard-nav-item">
+                        <i className="fas fa-user"></i>
+                        Profile
+                    </a>
+
+                    <a className="dashboard-nav-item">
+                        <i className="fas fa-video"></i>
+                        Monitor Exam
+                    </a>
+                    <a className="dashboard-nav-item active">
+                        <i className="fas fa-clipboard"></i>
+                        Publish Result
+                    </a>
+
+                    <div className={classname}>
+                        <a onClick={handleshow} className="dashboard-nav-item dashboard-nav-dropdown-toggle ">
+                            <i className="fas fa-plus"></i>
+                            Examination
+                        </a>
+
+                        <div className='dashboard-nav-dropdown-menu'>
+                            <a className="dashboard-nav-dropdown-item">
+                                <i className="fas fa-plus mr2"></i>
+                                Create Exam
+                            </a>
+
+                            <a className="dashboard-nav-dropdown-item">
+                                <i className="fas fa-eye mr2"></i>
+                                View Exam
+                            </a>
+
+                            <a className="dashboard-nav-dropdown-item">
+                                <i className="fas fa-pencil-alt mr2"></i>
+                                Update Exam
+                            </a>
+
+                            <a className="dashboard-nav-dropdown-item">
+                                <i className="fas fa-trash mr2"></i>
+                                Delete Exam
+                            </a>
                         </div>
                     </div>
-                    <a href="#" class="dashboard-nav-item"><i class="fas fa-cogs"></i> Settings </a>
-                    <div class="nav-item-divider"></div>
-                    <a
-                        href="#" class="dashboard-nav-item"><i class="fas fa-sign-out-alt"></i> Logout </a>
+
+                    <div className='dashboard-nav-dropdown'><a className="dashboard-nav-item dashboard-nav-dropdown-toggle"><i className="fas fa-users"></i> Users </a>
+                        <div className='dashboard-nav-dropdown-menu'>
+                            <a className="dashboard-nav-dropdown-item">
+                                All
+                            </a>
+
+                            <a className="dashboard-nav-dropdown-item">
+                                Subscribed
+                            </a>
+
+                            <a className="dashboard-nav-dropdown-item">
+                                Non-subscribed
+                            </a>
+
+                            <a className="dashboard-nav-dropdown-item">
+                                Banned
+                            </a>
+
+                            <a className="dashboard-nav-dropdown-item">
+                                New
+                            </a>
+                        </div>
+                    </div>
+
+                    
+                    <a className="dashboard-nav-item">
+                        <i className="fas fa-cogs"></i>
+                        Settings
+                    </a>
+
+                    <div className="nav-item-divider"></div>
+
+                    <a className="dashboard-nav-item">
+                        <i className="fas fa-sign-out-alt"></i>
+                        Logout
+                    </a>
+
                 </nav>
             </div>
-            <div class='dashboard-app'>
-                <header class='dashboard-toolbar'><a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a></header>
-                <div class='dashboard-content'>
-                    <div class='container'>
-                        <div class='card'>
-                            <div class='card-header'>
+            <div className='dashboard-app'>
+                <header className='dashboard-toolbar'>
+                    <a className="menu-toggle">
+                        <i className="fas fa-bars"></i>
+                    </a>
+                </header>
+
+                <div className='dashboard-content'>
+                    <div className='container'>
+                        <div className='card'>
+                            
+                            <div className='card-header'>
                                 <h1>Welcome back Jim</h1>
                             </div>
-                            <div class='card-body'>
+
+                            <div className='card-body'>
                                 <p>Your account type is: Administrator</p>
                             </div>
                         </div>
