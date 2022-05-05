8ce0852fe5e9eddb1a441065f8b7201318afcd72
Vedantikawagaj <vedantikawagaj@gmail.com>
2022-05-06 01:45:52 +0530

    Report logs

--- proctoring-system/Backend/app/routes/exam.routes.js
+++ proctoring-system/Backend/app/routes/exam.routes.js
@@ -27,5 +27,7 @@ module.exports = function(app) {
   app.put("/api/question/uq/:qid", controller.updateQuestion);
   app.get("/api/question/vpq/:qid", controller.viewParticularQuestion);
   app.post("/api/se", controller.searchExam);
-  app.post("/api/sendmail",controller.sendmail)
+  app.post("/api/sendmail",controller.sendmail);
+  app.post("/api/post-log",controller.generateLog);
+  app.get("/api/view-log/:eid", controller.viewLogOfExam);
 };
